311afd8ef1906cd65706f24e60a938827b9e86bd
eallenOP <elise.allen@op.ac.nz>
2023-09-12 17:10:08 +1200

    Fetch a dog image src url

---
+++ src/routes/+page.server.js
@@ -0,0 +1,22 @@
+/* The load function can be used to fetch data
+   as soon as the page loads, and pass it to the 
+   page to display in the front end */
+
+export async function load({ fetch }) {
+  const res = await fetch(`https://dog.ceo/api/breeds/image/random`);
+  const dog = await res.json();
+  return { dog };
+}
+
+/*
+
+The dog api above returns this:
+
+{
+    "message": "https://images.dog.ceo/breeds/sharpei/noel.jpg",
+    "status": "success"
+}
+
+so the dog object being returned to your +page.svelte will contain the message and status properties
+
+*/
