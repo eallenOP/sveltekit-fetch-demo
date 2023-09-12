1392906f5c5d666f0c7fa861b337168ca6023df7
eallenOP <elise.allen@op.ac.nz>
2023-09-12 17:10:20 +1200

    Display the random dog image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    // The data prop brings anything you fetched in the load function in here for use on the page
+    export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img src={data.dog.message} alt="Random dog">
