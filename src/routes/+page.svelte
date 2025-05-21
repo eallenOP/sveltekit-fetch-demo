67b79b7ff0375da5e3c39127b3b01d5ae91e09d9
eallen <elise.allen@op.ac.nz>
2025-05-22 11:45:37 +1200

    Update +page.svelte to Svelte 5 runes syntax for data props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     // The data prop brings anything you fetched in the load function in here for use on the page
-    export let data;
+    let { data } = $props();
     console.log(data); // You can see the data in the console using the browser's dev tools
 </script>
 
